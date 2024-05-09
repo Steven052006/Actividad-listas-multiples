3d6e964f875d102f757a72c33c369fa1dcb86cc6
Steven052006 <158522413+Steven052006@users.noreply.github.com>
2024-05-09 16:21:31 -0500

    Add files via upload

---
+++ agro.py
@@ -0,0 +1,88 @@
+'''Elaborar un programa en python que permita calcular el area de un lote y luego determine su valor dependiendo a las siguieten condiciones:
+1. Urbano:
+mt2 = 25.000
+permiso de construccion = 45% del valor 
+2. Comercial:
+mt2 = 60.000
+permiso de construccion = 75% del valor 
+3. Campestre:
+mt2 = 35.000
+permiso de construccion = 15% del valor
+'''
+UrbanoMt2 = 25000
+Permiso_urbano = 0.45
+ComercialMt2 = 60000
+Permiso_comercial = 0.75
+CampestreMt2 = 35000
+Permiso_campestre = 0.15
+
+ls_urbano = []
+ls_comercial = []
+ls_campestre = []
+
+import os
+
+def fnt_limpiar():
+    os.system('cls')
+    print('Autor: Bilgier Perea A.')
+    print('Fecha: 25 de abril del 2024\n')
+
+def fnt_registrar(nombre, ancho, largo, tipo_lote):
+    if tipo_lote == "Urbano":
+        valor_mt2 = UrbanoMt2
+        permiso_construccion = Permiso_urbano
+    elif tipo_lote == "Comercial":
+        valor_mt2 = ComercialMt2
+        permiso_construccion = Permiso_comercial
+    elif tipo_lote == "Campestre":
+        valor_mt2 = CampestreMt2
+        permiso_construccion = Permiso_campestre
+
+    area = ancho * largo
+    valor_lote = area * valor_mt2
+    valor_permiso = valor_lote * permiso_construccion
+    print(f'El valor del lote es: {valor_lote:.2f}')
+    print(f'El valor del permiso de construcción es: {valor_permiso:.2f}')
+    print(f'El valor total es: {valor_lote + valor_permiso:.2f}')
+
+def fnt_selector(op):
+    global sw
+    if op == '1':
+        nombre = input('Nombre: ')
+        ancho = float(input("Ingrese el ancho del lote (en metros): "))
+        largo = float(input("Ingrese el largo del lote (en metros): "))
+        tipo_lote = input('<<<< MENU DE LOTES >>>>>\n1. Urbano\n2. Comercial\n3. Campestre\n -> ')
+        if tipo_lote == "1":
+            tipo_lote = "Urbano"
+            R = {'nombre': nombre, 'Area': ancho * largo, 'Tipo de lote': tipo_lote}
+            ls_urbano.append(R)
+        elif tipo_lote == "2":
+            tipo_lote = "Comercial"
+            R = {'nombre': nombre, 'Area': ancho * largo, 'Tipo de lote': tipo_lote}
+            ls_comercial.append(R)
+        elif tipo_lote == "3":
+            tipo_lote = "Campestre"
+            R = {'nombre': nombre, 'Area': ancho * largo, 'Tipo de lote': tipo_lote}
+            ls_campestre.append(R)
+        fnt_registrar(nombre, ancho, largo, tipo_lote)
+        enter = input('<ENTER>')
+    elif op == '2':
+        fnt_limpiar()
+        print('Lotes Urbanos:')
+        for lote in ls_urbano:
+            print(lote)
+        print('Lotes Comerciales:')
+        for lote in ls_comercial:
+            print(lote)
+        print('Lotes Campestres:')
+        for lote in ls_campestre:
+            print(lote)
+        enter = input('Presione <ENTER> para continuar')
+    elif op == '3':
+        sw = False
+
+sw = True
+while sw == True:
+    fnt_limpiar()
+    op = input('\n\n<<<< MENU PRINCIPAL >>>>\n1. REGISTRAR LOTES -> \n2. MOSTRAR LOTES -> \n3. SALIR ->\n -> ')
+    fnt_selector(op)
